4ed21340b79392010f07194f7100df6855e2cbc0
Ahmed Elnemr <elnemr21092003@gmail.com>
2026-08-15 22:28:23 +0300

    edit cors config

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,8 +1,17 @@
-export const corsConfig = {
-    origin: [
+const allowedOrigins=[
         'http://localhost:5173',
         'https://full-stack-doctor-appointment-syste-bay.vercel.app',
         'https://full-stack-doctor-appointment-syste-fawn.vercel.app'
-    ],
+    ]
+
+
+export const corsConfig = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 };
